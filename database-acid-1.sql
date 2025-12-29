b36e733eb0b89cf076d0cec9bb1365784afd7f5a
fatahilah <mnabilfatahilah@gmail.com>
2025-12-30 05:06:28 +0700

    Delete database-acid-1.sql

--- database-acid-1.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE belajar_acid;
-
-USE belajar_acid;
-
-CREATE TABLE accounts (
-	id VARCHAR(100) NOT NULL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    balance BIGINT NOT NULL
-) ENGINE = InnoDB;
